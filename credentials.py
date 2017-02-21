9c0378c3de20147f624048fc52cddf274471d239
Kelden Dradul Dorji <keldenddorji1997@gmail.com>
2017-02-21 21:50:47 +0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = 'ACed555245f7d6921c2947f49525785074'
-auth_token = '3577a6a9dfdb02f8f9ae8369f8dce373'
-my_cell = '+97517955747'
-my_twilio = '+12674607290'
+account_sid = 'Check twilio'
+auth_token = 'Check twilio'
+my_cell = '+########'
+my_twilio = '+########'
